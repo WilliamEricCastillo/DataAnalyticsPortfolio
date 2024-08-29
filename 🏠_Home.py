ed40c297bbe7da7362a1732b46f03295fc0c1fab
willcastl3 <willcastl3@outlook.com>
2024-08-28 21:09:19 -0500

    created home page, displays page purposes, photo and short bio of self

---
+++ 🏠_Home.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.markdown("# Data Analytics Portfolio")
+
+st.divider()
+
+st.markdown("""
+    <h1 style='text-align: center;'> William Eric Castillo</h1>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    <div style='text-align: center;'>
+        <img src='https://i.ibb.co/yY62RjG/Cropped-Image.png' alt='Placeholder Image' style='width: 300px; height: 300px; border-radius: 50%;'>
+    </div>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    <div style='text-align: center;'>
+        <p style='font-size:25px; margin: 0;'>Masters of Science in Information Science</p>
+        <p style='font-size:20px; margin: 0; padding-bottom: 2%;'>Texas A&M International University '25</p>
+    </div>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    <div style='text-align: center; margin-left: 25%; margin-right: 25%;'>
+    Welcome to my Data Analytics portfolio! 
+    Here, I will highlight my projects and share valuable insights. 
+    I am currently working towards a Master’s degree in Information Science, 
+    where I am exploring a broad range of IT and business subjects, 
+    including project management and data analytics. 
+    Upon completing my degree, I will also earn a certificate in data analytics, 
+    further refining my skills in data manipulation and the development of 
+    data-intensive applications.
+    </div>
+""", unsafe_allow_html=True)
+
+st.divider()
+
+st.markdown("""
+    <div style='text-align: center;'>
+        <p style='font-size:20px; margin: 0; text-decoration: underline;'>Technologies Used</p>
+        <p style='font-size:20px; margin: 0;'>Python, Streamlit, Plotly</p>
+        <p style='font-size:16px; margin: 0;'>Data provided by Kaggle</p>
+    </div>
+""", unsafe_allow_html=True)
